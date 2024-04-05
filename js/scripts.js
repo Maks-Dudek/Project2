800aa099894a7bc7d14474bce3d5c753e8a6bd6c
Grasaia <sdugger@hawk.iit.edu>
2024-04-04 23:04:54 -0500

    corrected src and adding scripts

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,29 @@ function signup() {
   document.getElementById("signup");
   alert("Account Created. Thank you!");
 }
+
+function changeCity(cityCode) {
+  var cityNameElement = document.getElementById('cityname');
+  switch(cityCode) {
+      case 'chc':
+          cityNameElement.textContent = 'Chicago';
+          break;
+      case 'nch':
+          cityNameElement.textContent = 'North Chicagoland';
+          break;
+      case 'wcl':
+          cityNameElement.textContent = 'West Chicagoland';
+          break;
+      case 'sox':
+          cityNameElement.textContent = 'South Chicagoland';
+          break;
+      case 'nwi':
+          cityNameElement.textContent = 'Northwest Indiana';
+          break;
+      case 'nwc':
+          cityNameElement.textContent = 'Northwest Suburbs';
+          break;
+      default:
+          break;
+  }
+}
